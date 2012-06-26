1daec081d99c56d29da8f9fd201f1cf07aa689d1
ghpt15 <patwork15-01@yahoo.com>
2012-06-26 21:44:07 +0800

    updated phpmyadmin config file

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -28,11 +28,11 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'tunnel.pagodabox.com';
+$cfg['Servers'][$i]['host'] = 'tunnel.pagodabox.com:3306';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 /* Select mysql if your server does not have mysqli */
-$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['extension'] = 'mysql';
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
 /*
